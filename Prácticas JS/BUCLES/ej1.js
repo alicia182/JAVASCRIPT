1db282fab81f782a3b707a82c55e649bee94d949
Alicia Gonzalez <aliciaglo83@gmail.com>
2022-06-11 18:04:33 +0200

    3commit

---
+++ Prácticas JS/BUCLES/ej1.js
@@ -0,0 +1,6 @@
+let edad = parseInt(window.prompt('Introduce tu edad'))
+for (let i = 1 <= edad; i++){
+    console.log('feliz cumpleaños')
+}
+
+//revisar en video de 9 junio
---
+++ Prácticas JS/BUCLES/ej2.js
@@ -0,0 +1,14 @@
+let num = 0 parseInt(window.prompt('Introduce el número:'))
+
+let suma = 0
+
+
+
+for (let i = 1; i <= num; i++){
+    
+    suma += i     //operador de asignacion numerica, operan sobre si mismos, es lo mismo de suma 
+}
+
+console.log(suma)
+
+//revisar en video
